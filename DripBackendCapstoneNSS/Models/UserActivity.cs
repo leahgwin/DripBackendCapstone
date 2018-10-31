8c9a5bd82909d50fe118f0936e60e6d1053c3f1c
Leah <leahgwin@gmail.com>
2018-10-31 11:35:31 -0500

    added useractivity model

---
+++ DripBackendCapstoneNSS/Models/UserActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dripCapstone.Models
+{
+    public class UserActivity
+    {
+        [Key]
+        public int UserActivityId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date), Display(Name = "Date of Water Usage")]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public int Count { get; set; }
+    }
+}
