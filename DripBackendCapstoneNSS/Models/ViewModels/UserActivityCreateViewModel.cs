e080d85c3fe7725368df4edbe9bc51a7f963c00d
Leah <leahgwin@gmail.com>
2018-11-04 21:21:19 -0600

    made the drop down to select which activity to add. Now I'll need to add an input for the count

---
+++ DripBackendCapstoneNSS/Models/ViewModels/UserActivityCreateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using DripBackendCapstoneNSS.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DripBackendCapstoneNSS.Models.ViewModels
+{
+    public class UserActivityCreateViewModel
+    {
+            public List<SelectListItem> Activities { get; set; }
+            public SelectList ActivitiesList { get; set; }
+
+        //this is the dropdown for selecting which activity you want to create
+            public UserActivityCreateViewModel(ApplicationDbContext context)
+            {
+                Activities = context.Activity.Select(activity =>
+                new SelectListItem { Text = activity.Name, Value = activity.ActivityId.ToString() }).ToList();
+            }
+
+            public UserActivityCreateViewModel() { }
+    }
+}
+
+
+//in create: to show drop downs, you need two collections/dropdowns in the view model (select list)
