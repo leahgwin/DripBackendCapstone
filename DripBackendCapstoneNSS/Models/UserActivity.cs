56f7855afe0024c1f5f60ea0777664b427ee351e
Leah <leahgwin@gmail.com>
2018-11-04 20:07:05 -0600

    Made the dateTime only require the date. Added the Foreign Keys of user and activity onto model

--- DripBackendCapstoneNSS/Models/UserActivity.cs
+++ DripBackendCapstoneNSS/Models/UserActivity.cs
@@ -13,10 +13,17 @@ namespace DripBackendCapstoneNSS.Models
 
         [Required]
         [DataType(DataType.Date), Display(Name = "Date of Water Usage")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
         public DateTime Date { get; set; }
 
         [Required]
         [Display(Name = "How many times?")]
         public int Count { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+        public ICollection<Activity> Activities { get; set; }
     }
 }
