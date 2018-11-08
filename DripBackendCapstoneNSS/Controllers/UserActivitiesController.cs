71c6ce36548e829cfe632bf652639b3a870605c1
Leah <leahgwin@gmail.com>
2018-11-08 09:46:44 -0600

    fixed some headers and then made it to where only the user sees their logged water

--- DripBackendCapstoneNSS/Controllers/UserActivitiesController.cs
+++ DripBackendCapstoneNSS/Controllers/UserActivitiesController.cs
@@ -39,14 +39,18 @@ namespace DripBackendCapstoneNSS.Controllers
             User user = await GetCurrentUserAsync();
             var viewModel = new UserActivityListViewModel();
 
-            var userActivities = await _context.UserActivity.Include(u => u.Activity).Include(u => u.User).ToListAsync();
-            //loop through useractivities with linq statement to call method
+            var userActivities = await _context.UserActivity
+                .Where(u => u.User.Id == user.Id)
+                .Include(u => u.Activity)
+                .Include(u => u.User)
+                .ToListAsync();
+            //loop through useractivities with linq statement to call multiplication method
             foreach (UserActivity u in userActivities)
             {
                 u.SetLiterTotal();
             }
             viewModel.UserActivities = userActivities;
-            return View(viewModel);
+            return View(viewModel); 
 
 
         }
